38f6fe3d7de894375cae5b418dc2a5b92a8b44f3
ajoymaity <ajoy.maity@tarento.com>
2018-06-20 17:00:22 +0530

    Fixed folder structure

--- src/android/DownloadManager.java
+++ src/android/DownloadManager.java
@@ -1,4 +1,4 @@
-package downloadmanager;
+package com.tarento.downloadmanager;
 
 import android.content.Context;
 import android.net.Uri;
--- www/plugin.js
+++ www/plugin.js
@@ -1,13 +1,10 @@
 var exec = require('cordova/exec');
+var CLASS_NAME='DownloadManager';
+var METHOD_NAME='download';
 
-var PLUGIN_NAME = 'DownloadManager';
-
-var DownloadManager = {
-
-    download: function (path) {
-        exec(null, null, PLUGIN_NAME, "download", [path]);
-    },
-
+var dmanager={
+downloading: function(arg0, success, error) {
+    exec(success, error, CLASS_NAME, METHOD_NAME, [arg0]);
+}
 };
-
-module.exports = DownloadManager;
+module.exports=dmanager;
